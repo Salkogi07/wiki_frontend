733c80ef8961e554a2a7d2a1579696b163617501
Salkogi07 <kogisal099@gmail.com>
2024-12-03 19:04:08 +0900

    12_03

--- src/wikiPage.js
+++ src/wikiPage.js
@@ -1,13 +1,40 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
 import "./WikiPage.css";
 
 function WikiPage(){
     const {searchQuery} = useParams();
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`/api/bulids/${searchQuery}`);
+                if (response.ok) {
+                    const myData = await response.json();
+                    setData(myData);
+                }else {
+                    const errorMessage = await response.json();
+                    setError(errorMessage.message);
+                }
+            } catch(error){
+                setError("데이터를 가져오는데 오류가 발생하였습니다.");
+            }
+        };
+        fetchData();
+    }, [searchQuery])
     return(
         <div className="container">
             <h1>{searchQuery}</h1>
             <div className="article-container">
-                <h2>내용이 없습니다.</h2>
+                {error ? (
+                    <p>{error}</p>
+                ) : data ? (
+                    <p>{data}</p>
+                ) : (
+                    <p>데이터를 로드 중입니다.</p>
+                )}
             </div>
         </div>
     )
